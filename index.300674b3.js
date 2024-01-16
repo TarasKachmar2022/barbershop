e7a3c8c78bbe1bc7d7d96ebf6c881653edc4c1dd
TarasKachmar2022 <107129371+TarasKachmar2022@users.noreply.github.com>
2024-01-16 00:40:11 +0000

    Deploying to gh-pages from @ TarasKachmar2022/barbershop@4b23b500f7e6330ce971a85f130bf171335957c2 🚀

---
+++ index.300674b3.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#phone");new IMask(e,{mask:"+{38}(000)000-00-00"});
+//# sourceMappingURL=index.300674b3.js.map
---
+++ index.7f06414b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#phone");new IMask(e,{mask:"+{38}(000)000-00-00"})}();
+//# sourceMappingURL=index.7f06414b.js.map
