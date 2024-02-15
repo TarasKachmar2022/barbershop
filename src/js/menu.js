477ee2af72684cf91e536bf88e0562deb65128e8
Taras Kachmar <107129371+TarasKachmar2022@users.noreply.github.com>
2024-02-15 23:42:10 +0200

    fix

--- src/js/menu.js
+++ src/js/menu.js
@@ -14,3 +14,30 @@
     refs.body.classList.toggle('no-scroll');
   }
 })();
+
+(() => {
+  const mobileMenu = document.querySelector('[data-menu]');
+  const openMenuBtn = document.querySelector('[data-menu-btn]');
+  const body = document.querySelector('[data-page]');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    openMenuBtn.classList.toggle('active');
+
+    body.classList.toggle('no-scroll');
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.classList.remove('active');
+    body.classList.remove('no-scroll');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
